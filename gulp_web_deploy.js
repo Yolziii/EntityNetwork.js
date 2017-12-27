c4baccb5effd039e444a8f392d652a53d378fbe2
Yolziii <yolziii@gmail.com>
2017-12-27 06:46:54 +0200

    jsmin

--- gulp_web_deploy.js
+++ gulp_web_deploy.js
@@ -11,6 +11,7 @@ const zip = require('gulp-zip');
 const replace = require('gulp-replace');
 const mocha = require('gulp-mocha');
 const pump = require('pump');
+var minify = require('gulp-minify');
 //const imagemin = require('gulp-imagemin');
 
 //const babel = require('gulp-babel');
@@ -50,6 +51,14 @@ gulp.task('copySources', ['copyTestsJson'], function() {
     return gulp.src(path.join(source, '/**/*.js'))
         .pipe(replace(/\/\/ #Node.js[.\W\w]*\/\/ Node.js#/gi, ''))
         .pipe(concat(libraryFile))
+        .pipe(minify({
+            ext:{
+                src:'-debug.js',
+                min:'-min.js'
+            },
+            //exclude: ['tasks'],
+            //ignoreFiles: ['.combo.js', '-min.js']
+        }))
         .pipe(gulp.dest(destination));
 });
 
